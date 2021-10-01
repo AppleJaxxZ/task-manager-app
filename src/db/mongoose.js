f60fae462f8fd56b4dafaa2761872c52ba1d5b07
AppleJaxxZ <smackanalex@gmail.com>
2021-10-01 16:10:53 -0400

    Changed the way patch requets are made usining mongoose, created user authentication and security, created password hashes using bcrypt, updated userSchemas for mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -10,19 +10,3 @@ mongoose
   .catch(() => {
     console.log("Connection failed.");
   });
-
-// const Task = mongoose.model("Task", {
-//   task: {
-//     type: String,
-//     required: true,
-//   },
-//   completed: {
-//     type: Boolean,
-//     default: false,
-//   },
-//   description: {
-//     type: String,
-//     required: true,
-//     trim: true,
-//   },
-// });
