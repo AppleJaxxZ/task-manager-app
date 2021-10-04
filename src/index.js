025b6717addde197d13a0f303eeab61c30ebc215
AppleJaxxZ <smackanalex@gmail.com>
2021-10-04 11:45:57 -0400

    Created authentication tokens on login and unique user emails

--- src/index.js
+++ src/index.js
@@ -14,16 +14,15 @@ app.listen(port, () => {
   console.log("Your Server Is Alive on port: " + port);
 });
 
-const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const myFunction = async () => {
-  const password = "Red12345!";
-  const hashedPassword = await bcrypt.hash(password, 8);
+  const token = jwt.sign({ _id: "abc123" }, "thisismynewapp", {
+    expiresIn: "30 seconds",
+  });
+  console.log(token);
 
-  console.log(password);
-  console.log(hashedPassword);
-
-  const isMatch = await bcrypt.compare("Red12345!", hashedPassword);
-  console.log(isMatch);
+  const data = jwt.verify(token, "thisismynewapp");
+  console.log(data);
 };
 
 myFunction();
--- src/routers/user.js
+++ src/routers/user.js
@@ -14,6 +14,19 @@ router.post("/users", async (req, res) => {
   }
 });
 
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(400).send();
+  }
+});
+
 router.get("/users", async (req, res) => {
   try {
     const users = await User.find({});
