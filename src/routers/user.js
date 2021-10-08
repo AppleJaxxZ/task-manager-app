2eee97470e83db86f56c425123f5b20f49494170
AppleJaxxZ <smackanalex@gmail.com>
2021-10-08 14:07:25 -0400

    Finished with authentication, creating, deleteing, finding, patching, users and tasks and privacy for each user

--- src/routers/user.js
+++ src/routers/user.js
@@ -83,10 +83,6 @@ router.patch("/users/me", auth, async (req, res) => {
 
 router.delete("/users/me", auth, async (req, res) => {
   try {
-    // const user = await User.findByIdAndDelete(req.params.id);
-    // if (!user) {
-    //   return res.status(404).send();
-    // }
     await req.user.remove();
     res.send(req.user);
   } catch (e) {
